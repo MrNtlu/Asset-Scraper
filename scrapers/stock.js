e334f6f328fb3d6dad8f023e2d3da02ee5882b4b
MrNtlu <97burakfidan97@gmail.com>
2022-03-13 21:34:04 +0300

    Stock Market data scraper implemented. Puppeteer implemented. Functions separated. Minor bug fixes.

--- scrapers/stock.js
+++ scrapers/stock.js
@@ -32,10 +32,7 @@ async function getStocksData(url, stockCurrency, stockMarket, stockList, isRefre
     try {
         const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox", '--disable-setuid-sandbox'] });
         const page = await browser.newPage();
-        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
         await page.goto(url);
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        await page.screenshot({ path: `chromium.png` });
         await page.waitForSelector('#cross_rate_markets_stocks_1');
         await new Promise(resolve => setTimeout(resolve, 1500));
         const html = await page.content();
