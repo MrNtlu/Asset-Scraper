35b226660886560597f85932dc60972b02f64300
MrNtlu <97burakfidan97@gmail.com>
2022-06-24 21:18:00 +0300

    Commodity prevention added.

--- scrapers/commodity.js
+++ scrapers/commodity.js
@@ -2,10 +2,13 @@ const request = require("request-promise")
 const cheerio = require("cheerio");
 const { InvestingModel } = require("../mongodb");
 
+const commodityList = ["Gold", "Platinum", "Palladium", "Silver"];
+
 async function getCommodityPrices(investingList) {    
     const result = await request.get("https://markets.businessinsider.com/commodities");
     const $ = cheerio.load(result);
 
+    const tempCommodityCheckerList = []
     $($(".table")[1]).each((_, element) => {
         $(element).find($('tbody > tr')).each((_, innerElement) => {
             var commodityName;
@@ -17,24 +20,33 @@ async function getCommodityPrices(investingList) {
                     commodityPrice = parseFloat($(e).text().trim().replace(',', ''));
                 }
             });
-            investingList.push(
-                InvestingModel({
-                    _id: {
-                        symbol: getSymbolFromName(commodityName),
-                        type: "commodity",
-                        market: "Business Insider"
-                    },
-                    name: commodityName,
-                    price: commodityPrice,
-                    created_at: new Date()
-                })
-            );
+            if (commodityList.indexOf(commodityName) > -1) {
+                tempCommodityCheckerList.push(commodityName);
+                investingList.push(
+                    InvestingModel({
+                        _id: {
+                            symbol: getSymbolFromName(commodityName),
+                            type: "commodity",
+                            market: "Business Insider"
+                        },
+                        name: commodityName,
+                        price: commodityPrice,
+                        created_at: new Date()
+                    })
+                );   
+            }
         });
     });
 
-    await InvestingModel.deleteMany({
-        "_id.type": { $in: ["exchange", "commodity"]}
-    });
+    if (tempCommodityCheckerList.length == commodityList.length) {
+        await InvestingModel.deleteMany({
+            "_id.type": { $in: ["exchange", "commodity"]}
+        });
+    } else {
+        await InvestingModel.deleteMany({
+            "_id.type": { $in: ["exchange"]}
+        });
+    }
     await InvestingModel.insertMany(investingList);
 }
 
